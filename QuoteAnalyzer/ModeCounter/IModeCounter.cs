7e4f75a2b133fed9b7b7ab80f37ce18c0942b243
TorchukAndrii <andrii.torchuk2@gmail.com>
2025-09-08 10:10:36 +0300

    Move broadcaster to common solution. Add shared config

--- QuoteAnalyzer/ModeCounter/IModeCounter.cs
+++ QuoteAnalyzer/ModeCounter/IModeCounter.cs
@@ -5,13 +5,3 @@ public interface IModeCounter
     void Add(double value);
     double Mode();
 }
-
-public sealed class ModeCounterFactory
-{
-    public static IModeCounter Create(string algo)
-    {
-        return algo.Equals("Dictionary", StringComparison.OrdinalIgnoreCase)
-            ? new DictionaryModeCounter()
-            : new SpaceSavingModeCounter(1000);
-    }
-}
---
+++ QuoteAnalyzer/ModeCounter/ModeCounterFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace QuoteAnalyzer.ModeCounter;
+
+public sealed class ModeCounterFactory
+{
+    public static IModeCounter Create(string algo)
+    {
+        return algo.Equals("Dictionary", StringComparison.OrdinalIgnoreCase)
+            ? new DictionaryModeCounter()
+            : new SpaceSavingModeCounter(1000);
+    }
+}
